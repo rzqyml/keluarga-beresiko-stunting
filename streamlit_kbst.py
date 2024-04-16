a2a1bc748441e493ec93fa709210870a056b5d7f
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-04-16 10:41:32 +0700

    Update streamlit_kbst.py

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -21,33 +21,41 @@ default_values = {
     'terlalu_banyak_anak': '0'
 }
 
+# Mengecek dan menginisialisasi state jika belum ada
+if 'reset_flag' not in st.session_state:
+    st.session_state.reset_flag = False
+
+if 'state' not in st.session_state:
+    st.session_state.state = {
+        'sumber_air_minum_buruk': default_values['sumber_air_minum_buruk'],
+        'sanitasi_buruk': default_values['sanitasi_buruk'],
+        'terlalu_muda_istri': default_values['terlalu_muda_istri'],
+        'terlalu_tua_istri': default_values['terlalu_tua_istri'],
+        'terlalu_dekat_umur': default_values['terlalu_dekat_umur'],
+        'terlalu_banyak_anak': default_values['terlalu_banyak_anak']
+    }
+
 # Inisialisasi dataframe kosong
 input_result_df = pd.DataFrame()
 
-# Fungsi untuk input data
-def input_data():
-    # Input untuk pertanyaan-pertanyaan
-    with col1:
-        st.session_state.state['sumber_air_minum_buruk'] = st.text_input('Apakah Sumber Air Minum Buruk? (1=Ya, 0=Tidak)', st.session_state.state['sumber_air_minum_buruk'])
+# Input untuk pertanyaan-pertanyaan
+with col1:
+    st.session_state.state['sumber_air_minum_buruk'] = st.text_input('Apakah Sumber Air Minum Buruk? (1=Ya, 0=Tidak)', st.session_state.state['sumber_air_minum_buruk'])
 
-    with col2:
-        st.session_state.state['terlalu_muda_istri'] = st.text_input('Apakah Umur Istri Terlalu Muda? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_muda_istri'])
+with col2:
+    st.session_state.state['terlalu_muda_istri'] = st.text_input('Apakah Umur Istri Terlalu Muda? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_muda_istri'])
 
-    with col3:
-        st.session_state.state['terlalu_dekat_umur'] = st.text_input('Apakah Umur Suami & Istri Terlalu Dekat? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_dekat_umur'])
+with col3:
+    st.session_state.state['terlalu_dekat_umur'] = st.text_input('Apakah Umur Suami & Istri Terlalu Dekat? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_dekat_umur'])
 
-    with col1:
-        st.session_state.state['sanitasi_buruk'] = st.text_input('Apakah Sanitasi Buruk? (1=Ya, 0=Tidak)', st.session_state.state['sanitasi_buruk'])
+with col1:
+    st.session_state.state['sanitasi_buruk'] = st.text_input('Apakah Sanitasi Buruk? (1=Ya, 0=Tidak)', st.session_state.state['sanitasi_buruk'])
 
-    with col2:
-        st.session_state.state['terlalu_tua_istri'] = st.text_input('Apakah Istri Terlalu Tua? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_tua_istri'])
+with col2:
+    st.session_state.state['terlalu_tua_istri'] = st.text_input('Apakah Istri Terlalu Tua? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_tua_istri'])
 
-    with col3:
-        st.session_state.state['terlalu_banyak_anak'] = st.text_input('Apakah Memiliki Banyak Anak? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_banyak_anak'])
-
-# Tombol untuk input data
-if st.button('Mulai Input Data'):
-    input_data()
+with col3:
+    st.session_state.state['terlalu_banyak_anak'] = st.text_input('Apakah Memiliki Banyak Anak? (1=Ya, 0=Tidak)', st.session_state.state['terlalu_banyak_anak'])
 
 # Variabel untuk hasil prediksi
 kbst_diagnosis = ''
@@ -92,7 +100,3 @@ if not input_result_df.empty:
     st.write(input_result_df)
     csv = input_result_df.to_csv(index=False)
     st.download_button('Unduh Dataframe Hasil Prediksi', csv, 'predicted_results.csv')
-
-# Tombol untuk input data lagi
-if st.button('Input Data Lagi'):
-    input_data()
